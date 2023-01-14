95567a58bb2336696392efc363f9ac75ab08fe15
Martz <github@jmartz.de>
2023-01-14 21:28:34 +0100

    update spaceing mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import { faHeart } from "@fortawesome/free-solid-svg-icons";
 </script>
 
-<header class="mx-auto max-w-5xl my-2 flex justify-between">
+<header class="mx-auto max-w-5xl my-2 flex justify-between px-2 py-2">
   <h1 class="text-xl font-bold"><a href="/">Klotz-Imbiss.de</a></h1>
   <div class="nav space-x-3 mt-1 text-white">
     <a href="/impressum">Impressum</a>
@@ -12,9 +12,9 @@
   </div>
 </header>
 
-<main class="mx-auto max-w-5xl my-2"><slot /></main>
+<main class="mx-auto max-w-5xl my-2 px-2 py-2"><slot /></main>
 
-<footer class="mx-auto max-w-5xl my-2 mt-6">
+<footer class="mx-auto max-w-5xl my-2 mt-6 px-2 py-2">
   <div class="grid grid-cols-6">
     <div class="col-span-6 md:col-span-2">
       <a href="/impressum">Impressum</a> & <a href="/dateschutz">Datenschutz</a>
